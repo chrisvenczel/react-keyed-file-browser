8bdda2055def557d62dbfca6437de07e76a5c357
jukuly <vpare070@uottawa.ca>
2023-05-23 12:42:11 -0400

    update

--- src/filters/default.js
+++ src/filters/default.js
@@ -5,7 +5,7 @@ class Filter extends React.Component {
   static propTypes = {
     value: PropTypes.string.isRequired,
     updateFilter: PropTypes.func,
-    browserType: PropTypes.string
+    //browserType: PropTypes.string
   }
 
   handleFilterChange = (event) => {
@@ -17,7 +17,7 @@ class Filter extends React.Component {
     return (
       <input
         type="search"
-        placeholder={`Search ${this.props.browserType === 'sensor' ? 'Sensors' : this.props.browserType === 'sample' ? 'Samples' : ''}`}
+        placeholder={`Search ${/*this.props.browserType === 'sensor' ? 'Sensors' : this.props.browserType === 'sample' ? */'Samples' /*: ''*/}`}
         value={this.props.value}
         onChange={this.handleFilterChange}
       />
